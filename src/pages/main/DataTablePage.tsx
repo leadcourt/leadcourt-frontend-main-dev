bc5d465b20a14f7e8c53973c1b74298b070b678f
ladisamuel <yyemiteamazing@gmail.com>
2025-08-11 12:49:41 -0700

    fix

--- src/pages/main/DataTablePage.tsx
+++ src/pages/main/DataTablePage.tsx
@@ -417,7 +417,7 @@ export default function DataTablePage() {
       </Dialog>
       <div className=" grid grid-cols-12 pt3 overflow-hidden">
         {/* The filter section */}
-        <div className="lg:border-r border-r-gray-3fix00 lg:mr-1 col-span-12 lg:col-span-3 gap-3 lg:gap-0 p-3 max-h-[90vh] overflow-y-auto overflow-x-hidden static">
+        <div className="lg:border-r border-r-gray-300 lg:mr-1 col-span-12 lg:col-span-3 gap-3 lg:gap-0 p-3 max-h-[90vh] overflow-y-auto overflow-x-hidden static">
           {/* {keyFilters.Designation} */}
           <h2 className="font-bold col-span-12 text-2xl text-gray-600">
             Filters
